e3482af23386a06cd9a1ef4a95b84e26421b32da
차준호 <ckwnsgh4@naver.com>
2026-05-09 14:59:49 +0900

    first commit

---
+++ src/main/java/org/demo/aingthon/AingthonApplication.java
@@ -0,0 +1,13 @@
+package org.demo.aingthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AingthonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AingthonApplication.class, args);
+    }
+
+}
